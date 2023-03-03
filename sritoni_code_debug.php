1826e5e444c40ae9cb6b1f54b5b977f49d27504b
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2023-03-03 11:16:52 +0530

    dev

--- sritoni_code_debug.php
+++ sritoni_code_debug.php
@@ -118,7 +118,7 @@ function get_status_id_given_name( $status_name)
 
     foreach ($status_objects as $status_obj)
     {
-        if ( $status_name == $status_obj->name)
+        //if ( $status_name == $status_obj->name)
         {
             echo '<pre>';
             print("Status name - " . $status_name . " Corresponds to Status ID: " . $status_obj->id);
