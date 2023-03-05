788ae52f8162e3ab1df6b322afa044c2f8691c92
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2023-03-05 06:22:58 +0530

    dev

--- sritoni_code_debug.php
+++ sritoni_code_debug.php
@@ -25,6 +25,7 @@ function my_wpsc_change_ticket_status_callback( $ticket, $prev, $new, $customer_
 {
     echo '<pre>';
             print("Ticket ID - " . $ticket->id . " Old Status ID: " . $prev . " was changed to new status ID: " . $new);
+            print("Ticket cf student-first-name value is: " . $ticket->cust_25);
             print( "This information was derived from wpsc_change_status action callback");
             echo  '</pre>';
 }
